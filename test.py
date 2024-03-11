1e7e4baff44807262ee53cfc115c9dc543fbb6cc
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-03-11 20:31:11 +0300

    second commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-"""
+
 
 def solution (A, D):
     total_balance = 0
@@ -17,28 +17,8 @@ def solution (A, D):
 print(solution([100, 100 ,100, -10], ['2020-12-22', '2020-12-22', '2020-12-03', '2020-12-29']))
 
 
-"""
 
-def solution(A, D):
-    total_balance = 0
-    monthly_transactions = {}
-    total_fees = 0
 
-    for amount, date in zip(A, D):
-        month = int(date.split("-")[1])
-        monthly_transactions[month] = monthly_transactions.get(month, 0) + amount
-        total_balance += amount
 
-    for month, income in monthly_transactions.items():
-        if income < -100:
-            total_fees += 5
-        elif income < 100 and income > 0:
-            total_fees += 5
-
-    end_balance = total_balance - total_fees
-    return end_balance
-
-# Example usage:
-print(solution([100, 100, 100, -10], ['2020-12-22', '2020-12-22', '2020-12-03', '2020-12-29']))  # Output: 230
 
 
